043f8867dd5af26086fe7068dcde48ca6cdedc8b
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-02 16:02:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+</php
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '789483519:AAE8c1b3qbKnFoEGTsnDt6PskxhUbN1jHhY';
+$bot_username = 'username_bot';
+
+try {
+   
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key);
+
+    $telegram.handle();
+
+} 
+catch (Longman\TelegramBot\Exception\TelegramException $e) {
+   
+  echo $e.getMessage();
+}
+?>
