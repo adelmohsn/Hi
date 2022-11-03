617167cc45abd57e5630ee74bd89dbe9336b9323
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-03 03:50:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,7 @@ catch (Longman\TelegramBot\Exception\TelegramException $e) {
   echo $e.getMessage();
 }
 
-$result = Longman\TelegramBot\Request::sendMessage([
-    'chat_id' => '603508984',
-    'text'    => 'Your utf8 text 😜 ...',
-]);
+
 $result = Longman\TelegramBot\Request::sendMessage([
     'chat_id' => $this->getMessage()->getChat()->getId(),
     'text'    => 'Your utf8 text 😜 ...',
