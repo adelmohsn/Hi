01ff8c600675afee9e53f8c1ab69d183274c9108
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-05 20:20:51 +0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -67,8 +67,8 @@ $path=$this->url."/"."getme";
     if(curl_error($ch))
     var_dump(curl_error($ch));
     else return json_decode($rec);}
-public function getchat($id){
-$path=$this->url."/"."getchat?id={$id}";
+public function getchat($chat_id){
+$path=$this->url."/"."getchat?chat_id={$chat_id}";
     $ch=curl_init();
     curl_setopt($ch,CURLOPT_URL,$path );
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
