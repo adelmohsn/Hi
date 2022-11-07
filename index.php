dabf5597156e9e87bb3367a34f406217e353fd10
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-07 22:30:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,8 @@ else if (strtolower($text)=='\getchat'){
     $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$message->message_id,'text'=>getstring($A->method->getchat($chat_id))]);
 }
 
-else $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$message->message_id,'text'=>"sorry not found {$chat->type}"]);
-if($text=='hello' and ($chat->type=='group')){
+else $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$message->message_id,'text'=>"sorry not found type: {$chat->type}"]);
+if($text=='hello' and ($chat->type=="supergroup")){
 $A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this group:\n name: {$message->from->first_name}\n user_name: @{$message->from->username}\n id: {$message->from->id}",'reply_to_message_id'=>$message->message_id]);}
 else if($text=='hello' and $chat->type=='channel' )
 $A->method->sendmessage(['chat_id'=>$chat_id,"text'=>'welecome in this channel \n name: {$chat->first_name}\n link: {$chat->invite_link}",'reply_to_message_id'=>$message->message_id]);
