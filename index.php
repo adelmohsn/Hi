388b1aacbf5d9f81868ff3e8ed833b7551655887
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-09 17:37:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ else $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$messa
 if($text=='hello' and ($chat->type=="supergroup")){
 $A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this group:\n name: {$message->from->first_name}\n user_name: @{$message->from->username}\n id: {$message->from->id}",'reply_to_message_id'=>$message->message_id]);}
 else if($text=='hello' and $chat->type=='channel' )
-$A->method->sendmessage(['chat_id'=>$chat_id,"text'=>'welecome in this channel \n name: {$chat->first_name}\n link: {$chat->invite_link}",'reply_to_message_id'=>$message->message_id]);
+$A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this channel \n name: {$chat->first_name}\n link: {$chat->invite_link}",'reply_to_message_id'=>$message->message_id]);
 
 
 
