c66efc5babe54002c270d366ea7a334dc8cd9314
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-09 18:09:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ function getstring($data){
     if(is_object($value))
    $text.= getstring($value);
            else if(is_array($value))
-    $text.= implode($value);
+    $text.= $key.": ".implode($value)."\n";
     else
          $text.=$key." : ".$value."\n";
       }
