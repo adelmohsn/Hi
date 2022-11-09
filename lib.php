f9038fe56a3c05b835884e060a7848a2e544f201
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-09 18:06:44 +0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -8,13 +8,16 @@ class Telegram{
    public $Message;
 
 protected $API_KEY;
-public $update;
+protected $update;
 protected $url="https://api.telegram.org/bot";
 public   $method ;
 public function __construct($API_KEY){
 $this->API_KEY=$API_KEY;
 $this->url=$this->url.$API_KEY;
 $this->update=json_decode(file_get_contents('php://Input'));
+  if(!is_null($this->update->channel_post))
+$this->Message=$this->update->channel_post;
+else
 $this->Message=$this->update->message;
 $this->method=new method($API_KEY);
 
