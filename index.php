547f26b47d6de00d1cc2426333ad584451743493
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-10 17:50:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $chat=$message->chat;
 $chat_id=$chat->id;
 $text=$message->text;
 
+
 if(strtolower($text)=='/getme'){
  $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$message->message_id,'text'=>getstring($A->method->getme())]);
 }
