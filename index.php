006186cc6690789c55162e8d1cbb681d71bd3a9f
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-11 13:34:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,14 +20,11 @@ else if (strtolower($text)=='/getchat'){
 
 else if(strtolower($text)=='/start')$A->method->sendmessage(
 [
-'chat_id'=>$chat_id,'text'=>'hi','reply_markup'=>json_encode(
-    ['inline_keyboard'=>
-    [
-['text'=>'click here'
-    ,'url'=>'https://t.me/user?id={$message->from->id}'
-]
-]
-])
+'chat_id'=>$chat_id,'text'=>'hi','reply_markup'=>json_encode(  [  'inline_keyboard' => [
+        [
+            ['text' => 'COMMANDS', 'callback_data' => 'someString']
+        ]
+    ])
 ]);
 else if(strtolower( $text)=='hello' and ($chat->type=="supergroup")){
 $A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this group:\n name: {$message->from->first_name}\n user_name: @{$message->from->username}\n id: {$message->from->id}",'reply_to_message_id'=>$message->message_id]);}
