ba55004df9771fb3ddd5af954116af2e62b6b33b
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-13 16:37:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $chat=$message->chat;
 $chat_id=$chat->id;
 $text=$message->text;
 
-$type=json_encode($message->entities);
+$type=getstring( json_encode($message->entities));
 
     $A->method->sendmessage(['chat_id'=>$chat_id ,'reply_to_message_id'=>$message->message_id,'text'=>$type]);
 
