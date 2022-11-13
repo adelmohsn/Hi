cd08b9a1a0c9d080d3ace3362700696219ca1ef5
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-13 15:13:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -29,13 +29,9 @@ else if(strtolower($text)=='/start')$A->method->sendmessage(
         ]
 ]
     ])
-]);
-else if(strtolower( $text)=='hello' and ($chat->type=="supergroup")){
-$A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this group:\n name: {$message->from->first_name}\n user_name: @{$message->from->username}\n id: {$message->from->id}",'reply_to_message_id'=>$message->message_id]);}
-else if (strtolower( $text)=='hello'and ($chat->type=="private")  )
-$A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this bot \n name: {$chat->first_name}\n id:{$chat->id}\n user_name:{$chat->username}",'reply_to_message_id'=>$message->message_id]);
-else if($text=='hello' and $chat->type=='channel' )
-$A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in this channel \n name: {$chat->first_name}\n link: {$chat->invite_link}",'reply_to_message_id'=>$message->message_id]);
+]);else if(strtolower( $text)=='hello' ){
+$A->method->sendmessage(['chat_id'=>$chat_id,'text'=>"welecome in  {$chat->first_name}\n id: {$chat_id}",'reply_to_message_id'=>$message->message_id]);}
+
 
 else if(strtolower($text)=='delete')
                                    {$A->method->deletemessage(['chat_id'=>$chat_id,'message_id'=>$message->reply_to_message->message_id]);
