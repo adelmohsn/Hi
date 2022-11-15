c5d3b2a5d7a74c721258ebd0aa393e3976d915db
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-15 08:26:12 +0300

    Update method.php

--- method.php
+++ method.php
@@ -9,6 +9,26 @@ $this->token=$token;
 $this->url.=$token;
 }
 
+public function editmessage($data){
+
+return $this->editmsg("/editMessageText",$date);
+
+
+
+}
+protected function editmsg($method,$data){
+
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,$this->url.$method);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+$rec=curl_exec($ch);
+    if(curl_error($ch))
+    var_dump(curl_error($ch));
+    else return json_decode($rec);
+}
+  
+  
 public function deletemessage($data){ 
     return $this->delete("/deletemessage",$data);
 }
